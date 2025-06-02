766eba226ecc226297e8e372ffc97d66f6478c04
rahulkatlaparthi <69039932+rahulkatlaparthi@users.noreply.github.com>
2025-06-02 18:30:47 +0530

    Create hello_world.py

---
+++ dags/hello_world.py
@@ -0,0 +1,17 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+
+def say_hello():
+    print("Hello, Airflow from AKS!")
+
+with DAG(
+    dag_id='hello_world',
+    start_date=datetime(2024, 1, 1),
+    schedule_interval='@daily',
+    catchup=False,
+) as dag:
+    PythonOperator(
+        task_id='say_hello',
+        python_callable=say_hello
+    )
